79e9e523d0c4091364a4d8bf1c52b729644eb959
Vigneshwar <prasannavenkatesh@colanonline.com>
2022-03-31 19:58:23 +0530

    editing file

--- routes/get_product.js
+++ routes/get_product.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { DB_NAME, DB_URL, Client } = require("../constant/db_data");
 const Col = require("../constant/db_collections_name");
 
-router.post("/", (request, response) => {
-  Client.connect((err) => {
+router.post("/", async (request, response) => {
+  Client.connect(async (err) => {
     if (err) throw err;
 
     const DB = Client.db(DB_NAME);
-    // const collection = Client.db(DB_NAME).collection(Col.Available_products);
+    const collection = Client.db(DB_NAME).collection(Col.Available_products);
     // const collection = Client.db(DB_NAME).collection(Col.order_dimon);
-    const collection = Client.db(DB_NAME).collection(Col.candidate_bio_data);
+    // const collection = Client.db(DB_NAME).collection(Col.ordered_menus);
 
     var query = { price_of_product: 50 };
     var checkdata = { customer_address: "theni" };
@@ -23,7 +23,7 @@ router.post("/", (request, response) => {
     // collection.updateMany(checkdata, updatedata, function (err, res) {
     // collection.find(orderQuery).limit(10, 20).toArray(function (err, res) {
 
-    // collection.find(statusQuery).toArray(function (err, res) {
+    // collection.find(query, { projection: { items: 0 } }).toArray( function (err, res) {
     //     if (err) {
     //       response.json({ message: "failed", response: null }); Client.close();
     //     } else {
@@ -50,43 +50,75 @@ router.post("/", (request, response) => {
     //     Client.close();
     //   });
 
-    DB.collection(Col.candidate_marks)
-      .aggregate([
-        // {
-        //   $group: {
-        //     totalmarks:{$sum:["$Physics_Marks", "$Chem_Marks", "$Biology_Marks"]}
-        //   },
-        // },
-        {
-          $lookup: {
-            from: Col.candidate_bio_data,
-            localField: "Application_No",
-            foreignField: "Application_No",
-            as: "ph_marks",
-          },
-        },
-        {
-          $unwind: "$ph_marks",
-        },
-        {
-          $project: {
-            student_id: "$Application_No",
-            student_name: "$ph_marks.Name",
-            dob: "$ph_marks.DOB",
-            core_sub_marks:{
-              phy_mark: {$toInt:"$Physics_Marks"},
-              Chem_Marks: {$toInt:"$Chem_Marks"},
-              Biology_Marks: {$toInt:"$Biology_Marks"},
-            },
-            core_sub_total: {$sum: [ { $toInt: "$Physics_Marks" }, { $toInt: "$Chem_Marks" }, { $toInt: "$Biology_Marks" }, ],},
-          },
-        },
-      ])
-      .toArray(function (err, res) {
-        if (err) throw err;
-        response.json({ message: "success", response: res });
-        Client.close();
-      });
+    // var del_fee_cond = { $gt: ["$delivery_fee", 0] }
+    // DB.collection(Col.ordered_menus)
+    //   .aggregate([
+    //     // {$unwind: "$menus"},
+    //     {
+    //       $project: {
+    //         _id: 0,
+
+    //         // order_id:{$cond: [{ $gt: ["$delivery_fee", 0] } ,  "$order_id","$$REMOVE"] },
+    //         order_id:"$order_id",
+    //         order_type:{$cond: { if: { $gt: ["$delivery_fee", 0] }, then: "self_pickup", else:"delivery"}},
+    //         net_food_amount: { $cond: [ del_fee_cond, "$clear_amount", "$$REMOVE" ] },
+    //         delivery_address:{ $cond: [ del_fee_cond, "$restaurant_address.pickup_address", "$$REMOVE" ]},
+    //         // have_menuId_16: "$menus", //{ $cond: [ {$eq:["$menus[0].menu_id","16"]}, "yes", "no" ]}
+    //         non_veg_menus: {
+    //           $filter: {
+    //             input: "$menus",
+    //             as: "menus_array",
+    //             cond:{ $eq: ["$$menus_array.non_veg", "yes"] },// { $cond: [ {$eq:["$$menus_array.menu_id", 16]}, "yes", "no" ]}
+    //           },
+    //         },
+    //       },
+    //     },
+
+    //   ])
+    //   .toArray(function (err, res) {
+    //     if (err) throw err;
+    //     var newres = res.filter(value => Object.keys(value).length !== 0);
+    //     response.json({ message: "success", response: newres });
+    //     Client.close();
+    //   });
+
+    // for pagination
+    console.log("request.body ------>", request.body.page);
+    var perPage = 2;
+
+    var total_doc = await collection.count();
+    console.log("total docs ------>", total_doc);
+
+    var total_pages = Math.ceil(total_doc / perPage);
+    console.log("total pages ------>", total_pages);
+
+    var pageNumber = request.body.page == null ? 1 : request.body.page;
+    console.log("pageNumber ------>", pageNumber);
+
+    var startFrom = (pageNumber - 1) * perPage;
+    console.log("startFrom ------>", startFrom);
+
+    var res = await collection
+      .find({})
+      // .sort({ id: -1 })
+      .skip(startFrom)
+      .limit(perPage)
+      .toArray();
+
+      console.log("request.body.page:", Number(request.body.page)," total pages:",total_pages,)
+    var next_page =
+     ( Number(request.body.page) < total_pages) ? (Number(request.body.page) + 1) : total_pages;
+
+    var current_page = Number(request.body.page);
+
+    response.json({
+      message: "success",
+      total_pages,
+      next_page,
+      current_page,
+      response: res,
+    });
+
   });
 });
 
